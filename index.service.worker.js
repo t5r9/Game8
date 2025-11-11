0db0fe94e737de197dc7e7fbde195da8e03bffa4
12345ff <12345t5r9@gmail.com>
2025-11-12 05:09:40 +0900

    update4

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762891229|50786860';
+const CACHE_VERSION = '1762891714|535725989';
 /** @type {string} */
 const CACHE_PREFIX = 'Game8-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
